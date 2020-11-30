bdb149f2eb6a3705ccfe47820dcebf084c0e2cf0
web-deVVV <dew11911x@gmail.com>
2020-11-30 21:08:10 +0100

    general refactoring

---
+++ precache-manifest.44dd9b720c2951d6567e1eb68886f24b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7bd4fc55b43dcfc5a876760a4b2bce",
+    "url": "./index.html"
+  },
+  {
+    "revision": "894084ac7a55ce302f4f",
+    "url": "./static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "894084ac7a55ce302f4f",
+    "url": "./static/js/2.52182f2d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.52182f2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a44880e93726515b5261",
+    "url": "./static/js/main.d15655a1.chunk.js"
+  },
+  {
+    "revision": "e41721af2271558352cd",
+    "url": "./static/js/runtime-main.7e20be2c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-polluted-cities-app/precache-manifest.6e360191ddc611f41f93c632646edc85.js"
+  "./precache-manifest.44dd9b720c2951d6567e1eb68886f24b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-polluted-cities-app/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
